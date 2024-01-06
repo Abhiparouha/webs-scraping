4c2d39139a329173669422288b6dc1c5cdf831eb
Abhiparouha <155717254+Abhiparouha@users.noreply.github.com>
2024-01-06 16:59:12 +0530

    Add files via upload

--- BEAUTFULSOUP/webscraping.py
+++ BEAUTFULSOUP/webscraping.py
@@ -3,19 +3,19 @@ from bs4 import BeautifulSoup
 url ="https://www.amazon.in/b/?_encoding=UTF8&node=15325111031&pd_rd_w=z7mBk&content-id=amzn1.sym.b25e0925-6d5c-48be-9247-464080b72908&pf_rd_p=b25e0925-6d5c-48be-9247-464080b72908&pf_rd_r=FGW555MN43TBBMTJDW51&pd_rd_wg=9KGPE&pd_rd_r=06b79353-01f7-416c-8cd7-4037a4611a7b"
 r = requests.get(url)
 htmlContent = r.content
-# print(htmlContent)
+print(htmlContent)
 soup = BeautifulSoup(htmlContent, 'html.parser')
-# print(soup.prettify)
+print(soup.prettify)
 title=soup.title
-# print(type(title))
-# print(type(soup))
-# print(type(title.string))
+print(type(title))
+print(type(soup))
+print(type(title.string))
 # get the title of the paragraphs from the page
 paras = soup.find_all('p')
 # print(paras)
 # get the title of the anchor tags from the page
 anchors= soup.find_all('a')
-# print(anchors)
+print(anchors)
 
 # get first element in the html pages
 print(soup.find('p'))
